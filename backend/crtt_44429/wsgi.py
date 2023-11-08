03ca87caccdac5e51ac14a144c9e25eb68156736
Crowdbotics <team@crowdbotics.com>
2023-11-08 23:26:33 +0000

    Initial commit

---
+++ backend/crtt_44429/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for crtt_44429 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "crtt_44429.settings"
+)
+
+application = get_wsgi_application()
